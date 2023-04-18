45fac9b434b7171d5a9e1bcab5465ca42d6957e5
Alexandre Beauchamp <abeauchamp@donetechno.com>
2023-04-18 00:32:43 -0400

    Added parameter and component

---
+++ src/blazor/Client/Components/CounterComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLunchShare.Client.Components;
+
+public partial class CounterComponent
+{
+    private int currentCount = 0;
+
+    [Parameter]
+    [EditorRequired]
+    public int InitialCount { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        currentCount = InitialCount;
+    }
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
---
+++ src/blazor/Client/Pages/Details.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLunchShare.Client.Pages;
+
+public partial class Details
+{
+    [Parameter]
+    public int Id { get; set; }
+
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public string Name { get; set; } = default!;
+}
